ced34ace4aa81ef662fc062b7bd99f1ec88fb6d0
Davi Alves <davicdsalves@gmail.com>
2017-08-14 00:43:58 -0300

    add create, update and delete methods for product.

--- src/main/java/com/exam/store/controller/ProductController.java
+++ src/main/java/com/exam/store/controller/ProductController.java
@@ -4,10 +4,15 @@ import com.exam.store.controller.dto.ProductDTO;
 import com.exam.store.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -21,7 +26,33 @@ public class ProductController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> get(@PathVariable Long id) {
-        Optional<ProductDTO> categoryDTO = productService.findById(id);
-        return categoryDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        Optional<ProductDTO> productDTO = productService.findById(id);
+        return productDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PutMapping
+    public ResponseEntity<ProductDTO> create(@RequestBody @Valid ProductDTO request) {
+        ProductDTO dto = productService.save(request);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id,
+                                              @RequestBody ProductDTO request) {
+        if (productService.exists(id)) {
+            ProductDTO ProductDTO = productService.update(id, request);
+            return ResponseEntity.ok(ProductDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        if (productService.exists(id)) {
+            productService.delete(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
 }
--- src/main/java/com/exam/store/service/ProductService.java
+++ src/main/java/com/exam/store/service/ProductService.java
@@ -2,7 +2,9 @@ package com.exam.store.service;
 
 import com.exam.store.controller.dto.ProductDTO;
 import com.exam.store.factory.DTOFactory;
+import com.exam.store.model.Category;
 import com.exam.store.model.Product;
+import com.exam.store.repository.CategoryRepository;
 import com.exam.store.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,17 @@ import java.util.Optional;
 @Service
 public class ProductService {
     private ProductRepository repository;
+    private CategoryRepository categoryRepository;
     private DTOFactory factory;
 
-    public ProductService(ProductRepository repository, DTOFactory factory) {
+    public ProductService(ProductRepository repository, CategoryRepository categoryRepository, DTOFactory factory) {
         this.repository = repository;
+        this.categoryRepository = categoryRepository;
         this.factory = factory;
+    }
+
+    public boolean exists(Long id) {
+        return repository.exists(id);
     }
 
     public Optional<ProductDTO> findById(Long id) {
@@ -29,10 +37,36 @@ public class ProductService {
         return factory.createProductDTOs(products);
     }
 
-    //melhorar para ser -> exist by category id
-    public Optional<ProductDTO> findByCategoryId(Long id) {
-        Optional<Product> product = repository.findFirstByCategoryId(id);
-        return product.map(factory::createDTO);
+    public ProductDTO save(ProductDTO dto) {
+        Category category = validateProduct(dto);
+        Product Product = new Product(dto.getName(), category);
+        return save(Product);
+    }
+
+    public ProductDTO update(Long id, ProductDTO request) {
+        Category category = validateProduct(request);
+        Product product = repository.findOne(id);
+        product.setName(request.getName());
+        product.setCategory(category);
+        return save(product);
+    }
+
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+    private Category validateProduct(ProductDTO dto) {
+        Long categoryId = dto.getCategory().getId();
+        Optional<Category> validateCategory = categoryRepository.findById(categoryId);
+        if (!validateCategory.isPresent()) {
+            String errorMessage = "There is no category with id %d";
+            throw new IllegalArgumentException(String.format(errorMessage, categoryId));
+        }
+        return validateCategory.get();
+    }
+
+    private ProductDTO save(Product Product) {
+        return factory.createDTO(repository.save(Product));
     }
 
 

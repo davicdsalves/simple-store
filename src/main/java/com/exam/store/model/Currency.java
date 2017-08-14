2fc1f823e7a87d85916bdb96de93958eab4b2163
Davi Alves <davicdsalves@gmail.com>
2017-08-14 18:30:25 -0300

    add list of currencies accepted by fixer api.

---
+++ src/main/java/com/exam/store/model/Currency.java
@@ -0,0 +1,44 @@
+package com.exam.store.model;
+
+public enum Currency {
+    AUD,
+    BGN,
+    BRL,
+    CAD,
+    CHF,
+    CNY,
+    CZK,
+    DKK,
+    GBP,
+    HKD,
+    HRK,
+    HUF,
+    IDR,
+    ILS,
+    INR,
+    JPY,
+    KRW,
+    MXN,
+    MYR,
+    NOK,
+    NZD,
+    PHP,
+    PLN,
+    RON,
+    RUB,
+    SEK,
+    SGD,
+    THB,
+    TRY,
+    USD,
+    ZAR,
+    UNKNOWN;
+
+    public static Currency get(String name) {
+        try {
+            return Currency.valueOf(name);
+        } catch (IllegalArgumentException ex) {
+            return Currency.UNKNOWN;
+        }
+    }
+}
